b9421169acaae7aae92e4e8e392755d2f4ee4df1
Ben Wilson <Benjamin.wilson-1@colorado.edu>
2019-06-19 16:12:04 -0400

    Add files via upload

---
+++ WeekBChallenge/Wednesday.java
@@ -0,0 +1,11 @@
+package weekBActivity;
+
+public class Wednesday{
+	public static void main(String[] args) {
+        String string = "stringing it together";
+        for(int i=string.length() - 1;i>=0;i--)
+            {
+        	System.out.print(string.charAt(i));
+            }
+    }
+}
