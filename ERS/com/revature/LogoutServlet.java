80deafa818d31f5eda63ebe425170b96d9694c57
Ben Wilson <Benjamin.wilson-1@colorado.edu>
2019-07-30 07:36:08 -0400

    Create LogoutServlet.java

---
+++ ERS/com/revature/LogoutServlet.java
@@ -0,0 +1,25 @@
+package com.revature.servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  public LogoutServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("login.html").include(request, response);
+		HttpSession session = request.getSession(false); 
+		if (session != null) {
+			System.out.println("invalid");
+			session.invalidate();
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
