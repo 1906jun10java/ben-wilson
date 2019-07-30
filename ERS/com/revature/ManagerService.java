fecfa06cd2fade8e39fccd9df2c49026815ab3f3
Ben Wilson <Benjamin.wilson-1@colorado.edu>
2019-07-30 07:33:51 -0400

    Update ManagerService.java

--- ERS/com/revature/ManagerService.java
+++ ERS/com/revature/ManagerService.java
@@ -1,1 +1,27 @@
-
+package com.revature.service;
+import java.util.ArrayList;
+import com.revature.beans.Employees;
+import com.revature.beans.Requests;
+import com.revature.dao.DAOImpl;
+public class ManagerService {
+	DAOImpl pd = new DAOImpl();
+	public ArrayList<Requests> viewPending(int userId){ 
+		ArrayList<Requests> list =pd.pendingRequests(userId);
+		return list;
+	}
+	public ArrayList<Requests> resolved(int userId){
+		ArrayList<Requests> list =pd.resolvedRequests(userId);
+		return list;
+	}
+	public ArrayList<Employees> myEmps(int userId){
+		ArrayList<Employees> list = pd.myEmployees(userId);
+		return list;
+	}
+	public ArrayList<Requests> theirs(int userId, int id){
+		ArrayList<Requests> list =pd.theirRequests(userId, id);
+		return list;
+	}
+	public void resolve(int reqId, String status) {
+		pd.resolveRequests(reqId, status);
+	}
+}
