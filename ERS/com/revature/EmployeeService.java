f27a036f0216747fd93f98bee5ac4ac8b765db84
Ben Wilson <Benjamin.wilson-1@colorado.edu>
2019-07-30 07:31:31 -0400

    Update EmployeeService.java

--- ERS/com/revature/EmployeeService.java
+++ ERS/com/revature/EmployeeService.java
@@ -1,1 +1,31 @@
-
+package com.revature.service;
+import java.util.ArrayList;
+import com.revature.beans.Employees;
+import com.revature.beans.Requests;
+import com.revature.dao.DAOImpl;
+public class EmployeeService {
+	DAOImpl pd = new DAOImpl();
+	
+	public Employees viewInfo(int userId) {
+		Employees e = pd.myInfo(userId);
+	return e;
+	}
+	public ArrayList<Requests> viewAll(int userId){
+		return pd.myRequests(userId);
+	}
+	public ArrayList<Requests> viewPending(int userId){ 
+		ArrayList<Requests> list =pd.myPendingRequests(userId);
+		return list;
+	}
+	public ArrayList<Requests> viewResolved(int userId){
+		ArrayList<Requests> list = pd.myResolvedRequests(userId);
+		return list;
+	}
+	public boolean updateInfo(String newFName, String newLName,int userId) {
+	pd.updateMyInfo(newFName, newLName, userId); 
+	return true;
+	}
+	public void newReq(int userId, float amt, String cat, String detail) {
+		pd.newRequests(pd.myInfo(userId), amt,  cat, detail);
+	}
+}
