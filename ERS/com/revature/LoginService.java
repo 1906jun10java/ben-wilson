035dd4fe59c6b16a7136aab9db0e58598920abfc
Ben Wilson <Benjamin.wilson-1@colorado.edu>
2019-07-30 07:33:07 -0400

    Update LoginService.java

--- ERS/com/revature/LoginService.java
+++ ERS/com/revature/LoginService.java
@@ -1,1 +1,22 @@
-
+package com.revature.service;
+import com.revature.beans.Employees;
+import com.revature.beans.Login;
+import com.revature.dao.DAOImpl;
+public class LoginService {
+	DAOImpl pd = new DAOImpl();
+	public Employees checkLogin(Login log) {
+		Employees e =null;
+		String pass = log.getPass();
+		String user =log.getuName();
+		int userId = pd.login(user, pass);
+		if (userId!=0) {
+			e = pd.myInfo(userId);
+		}return e;
+	}
+	public boolean isManager(int id) {
+		int x =pd.myEmployees(id).size();
+		
+		if(x>0) {
+			return true;
+		}else return false;
+	}
